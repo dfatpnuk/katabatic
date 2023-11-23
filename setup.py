06436648dd43e156ef7b813737fc8b0bd2a43fe2
jblackw <135182569+jblackw@users.noreply.github.com>
2023-11-23 21:02:11 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+% pip install wheel
+% pip install setuptools
+% pip install twine
 
+from setuptools import find_packages, setup
+
+setup(
+    name='katabatic',
+    packages=find_packages(include=['katabatic']),
+    version='0.1.0',
+    description='An open source framework for tabular data generation',
+    author='Nayyar Zaidi, Jaime Blackwell',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+)
