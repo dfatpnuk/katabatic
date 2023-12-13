b5c6d2dd08beac79fa1c7e582abd4a0d757719e4
Jaime <jaime.shae.blackwell@gmail.com>
2023-12-13 13:10:01 +1100

    New notebook added to test the library.

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@
 % pip install setuptools
 % pip install twine
 
+import importlib # are these 
+import os
+import platform
+import shutil
+import sys
+import traceback   # necessary?
+
+from os.path import join
 from setuptools import find_packages, setup
 
 setup(
     name='katabatic',
     packages=find_packages(include=['katabatic']),
-    version='0.1.0',
+    version='0.0.1',
     description='An open source framework for tabular data generation',
-    author='Nayyar Zaidi, Jaime Blackwell',
+    author='Jaime Blackwell, Nayyar Zaidi',
     install_requires=[],
     setup_requires=['pytest-runner'],
 )
