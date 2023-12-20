eae2c78da431ff0faf64409972bd0a48746d399d
Jaime <jaime.shae.blackwell@gmail.com>
2023-12-21 10:17:32 +1100

    Added params to the base class

---
+++ katabatic/models/dummy.py
@@ -0,0 +1,9 @@
+# A dummy datagen model written by Jaime Blackwell
+
+import model   # Import the abstract base class
+
+class DummyModel(model):
+    def __init__(self,):
+
+
+    def fit(self, x, y, )
--- katabatic/models/model.py
+++ katabatic/models/model.py
@@ -1,21 +1,47 @@
+"""Abstract Base class for all models."""
+
+# Author: Jaime Blackwell
+# License: 
+
 from abc import ABC, abstractmethod
 
 class Model(ABC):
 
     @abstractmethod
     def __init__(self, *args, **kwargs):
-        pass
+
+        '''
+                    *** MODEL CLASS PARAMETERS ***
+        ------------------------------------------------------------
+        weights : initial weights for data generation. Default = None
+        epochs : number of epochs to train the model. Default = None
+        batch_size : batch size for training. Default = None
+        self.x : Training set for fitting to the model
+        self.Y : 
+        -------------------------------------------------------------
+        '''
+        self.weights = None
+        self.epochs = None
+        self.batch_size = None
+        self.x = None
+        self.Y = None
+        self.k = 0
 
     @abstractmethod
-    def fit():
-        pass
+    def fit(self, x, Y, k=self.k, batch_size=self.batch_size, epochs=self.epochs, **kwargs):    # sklearn-style fit method
+        self.x = x
+        self.Y = Y
+
+        return NotImplementedError('model.fit() must be defined in the concrete class')
 
     @abstractmethod
-    def generate():
-        pass
+    def generate(self, size=None, **kwargs):
+
+        return NotImplementedError('model.generate() must be defined in the concrete class')
 
     @abstractmethod
-    def evaluate():
-        pass
+    def evaluate(self, x, Y, classifier):
+
+        return NotImplementedError('model.evaluate() must be defined in the concrete class')
 
 
