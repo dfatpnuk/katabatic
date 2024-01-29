8e318e2b6bbbb359e1ea915ca283afd001245923
Jaime <jaime.shae.blackwell@gmail.com>
2024-01-29 14:55:01 +1100

    Update ganblr_adapter

--- katabatic/models/ganblr/ganblr_adapter.py
+++ katabatic/models/ganblr/ganblr_adapter.py
@@ -15,7 +15,7 @@ class GanblrAdapter():
     
     def fit(self, X_train, y_train, k=0, epochs=10, batch_size=64):   #TODO: remove hard coded numbers
         self.model.fit(X_train, y_train, k, batch_size=batch_size, epochs=epochs) # TODO: train self.model on the input data
-        return self.model
+        return self.model   # Don't really need to return anything. 
 
     def generate():
         pass
