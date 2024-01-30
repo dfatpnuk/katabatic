d1d0ca4e6e81336fa61fcf554e37fafb1360a802
Jaime <jaime.shae.blackwell@gmail.com>
2024-01-31 10:55:18 +1100

    Updated generate() and added TODOs

--- katabatic/models/ganblr/ganblr_adapter.py
+++ katabatic/models/ganblr/ganblr_adapter.py
@@ -6,16 +6,27 @@ class GanblrAdapter():
 
     def __init__(self):
         self.model = GANBLR() # Initialise and return an instance of the ganblr model. 
+        self.training_sample_size = 0
         return self.model
 
+    #TODO: add exception handling to load()
     def load(self, data_pathname):
         data = pd.DataFrame
         print("Loading Data...")
         return data
     
+    #TODO: add exception handling to fit()
     def fit(self, X_train, y_train, k=0, epochs=10, batch_size=64):   #TODO: remove hard coded numbers
+        
         self.model.fit(X_train, y_train, k, batch_size=batch_size, epochs=epochs) # TODO: train self.model on the input data
-        return self.model   # Don't really need to return anything. 
+        self.training_sample_size = X_train.len()
+        return   # Don't need to return anything. 
 
-    def generate():
-        pass
+    #TODO: add exception handling to generate()
+    def generate(self, size=None):  #Modify so that if size is not specified, default is the training sample size.
+        
+        if size==None: 
+            size = self.training_sample_size
+
+        generated_data = self.model.sample(size)
+        return generated_data
