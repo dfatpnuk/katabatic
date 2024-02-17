b3a65b6164f853d8797c2dea7d7af01bf6c12fa6
Jaime <jaime.shae.blackwell@gmail.com>
2024-02-17 22:15:30 +1100

    Update katabatic_spi

--- katabatic/katabatic_spi.py
+++ katabatic/katabatic_spi.py
@@ -1,8 +1,8 @@
-# The Katabatic Service Provider Interface (SPI) provides an abstract base class (ABC) for all model adapters to implement.
+# The Model Service Provider Interface (SPI) provides an abstract base class (ABC) for all model adapters to implement.
 
 from abc import ABC, abstractmethod
 
-class KatabaticSPI(ABC):
+class KatabaticModelSPI(ABC):
 
     @abstractmethod
     def load_model(): #Load the model
@@ -20,6 +20,26 @@ class KatabaticSPI(ABC):
     def generate(self): #Generate synthetic data
         pass
 
+
+#For the Katabatic Metric SPI, the input must be data/a model and the output must be a result.
+# Each Evaluation method must be applicable to tabular data.
+class KatabaticMetricSPI(ABC):
+
+    @abstractmethod
+    def evaluate(): #Load the model
+        pass
+
+    @abstractmethod
+    def load_data(): #Load data
+        pass
+
+    @abstractmethod
+    def fit(self):  #Fit model to data
+        pass
+
+    @abstractmethod
+    def generate(self): #Generate synthetic data
+        pass
 
         
     
